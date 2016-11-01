df622aa3064bbc6aa444d5a2d66226f437e2c8df
adamjung <ajung91@hotmail.com>
2016-10-31 19:50:01 -0700

    Partner programming with Harris, finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,30 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
+//document.body, element.childNodes, and element.classList
+
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  
+  var helper = function(className, resultList, node) {
+    var children = node.childNodes;
+    var classList = node.classList;
+    
+
+    if (!children) { // base case : solo node with no children
+      if (classList && classList.contains(className)) { 
+        resultList.push(node); 
+      }
+    } else if (classList && classList.contains(className)) {
+      resultList.push(node);
+    } 
+    for (var key in children) {
+      helper(className, resultList, children[key]);
+    }
+  };
+
+  helper(className, result, document.body);
+  return result;
 };
