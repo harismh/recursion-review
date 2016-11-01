6ea20637c94b928ef91bd8a7a8fcc58b0d543b90
harismh <harismh@icloud.com>
2016-11-01 11:26:03 -0700

    Pair programming with Adam finished stringifyJSON implementation

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  var stringify = function(obj) {
+    if (obj === null) {
+      return 'null';
+    } else if (typeof obj == 'string') {
+      return '"' + obj + '"';
+    } else if (obj === undefined) {
+      return 'undefined';
+
+    } else if (typeof obj == 'object') {
+      var temp = [];
+
+      if (Array.isArray(obj)) {
+        for (var i = 0; i < obj.length; i++) {
+          temp.push(stringifyJSON(obj[i]));
+        }
+        return '[' + temp + ']'; 
+      } else {
+        for (var key in obj) {
+          if (typeof obj[key] != 'function' && obj[key] !== undefined) {
+            temp.push(stringify(key) + ':' + stringify(obj[key]));
+          }
+        }
+
+        return '{' + temp + '}';
+      }
+    } else {
+      return obj.toString();
+    }
+  };
+
+  return stringify(obj);
 };
