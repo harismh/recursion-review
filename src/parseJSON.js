acf378b97e0d962ca7d9fd9454a2478ee610df16
harismh <harismh@icloud.com>
2016-11-01 18:37:53 -0700

    Some edit

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,39 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var objectHelper = function(json) {
+    var container = {};
+    var keyIndex;
+    var regex = /(?:\d*\.)?[a-zA-Z\d]+/igm;
+
+    // build a key value
+    var regexd = json.match(regex);
+
+    for (var i = 0; i < regexd.length; i++) {
+      // can we assume 
+      if (i % 2 === 0) {
+        keyIndex = regexd[i];
+      } 
+      if (i % 2 === 1) {
+        container[keyIndex] = parseJSON(regexd[i]);
+      }
+    }
+
+    return container;
+  };
+
+  if (currentChar === '{'){
+    currentChar++;
+    objectHelper(json);
+  }
+  // cases: object, array, number, string, 
+  return objectHelper(json);
+
+  // var result;
+
+  // for (var i = 0; i < json.length; i++) {
+  //   if (json[i] === '{') {
+  //     objectHelper(json.substring(i));
+  //   }
+  // }
 };
